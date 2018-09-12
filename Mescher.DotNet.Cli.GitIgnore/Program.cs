f2413d1ee0a01a673cec314b9067080ad72d336c
Mark Escher <mark-escher@uiowa.edu>
2018-09-11 20:39:09 -0500

    Created project structure

---
+++ Mescher.DotNet.Cli.GitIgnore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mescher.DotNet.Cli.GitIgnore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
